fc61d6cbeb1889fdb7f66fa09d99d9bf4d0c5fe7
jack <giac290595@gmail.com>
2018-10-02 12:55:30 +0100

    added f to export data from txt into a dictionary

--- Rule_Extractor.py
+++ Rule_Extractor.py
@@ -17,3 +17,30 @@ def matrix_to_csv(file):
         output.write("\n")
 
 
+def values_to_dictionary(file):
+
+    values_dict = dict()
+    values_dict.__setitem__("age", [])
+    values_dict.__setitem__("spectacle_prescription", [])
+    values_dict.__setitem__("astigmatic", [])
+    values_dict.__setitem__("tpr", [])
+    values_dict.__setitem__("lenses_target", [])
+
+    file = open(file)
+
+    for line in file:
+        values_dict["age"].append(int(line.split()[0]))
+        values_dict["spectacle_prescription"].append(int(line.split()[0]))
+        values_dict["astigmatic"].append(int(line.split()[0]))
+        values_dict["tpr"].append(int(line.split()[0]))
+        values_dict["lenses_target"].append(int(line.split()[0]))
+
+    # print(values_dict["age"])
+
+
+def main():
+
+    values_to_dictionary('lenses.txt')
+
+
+main()
