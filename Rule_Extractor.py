680c5522d51e2ae70d55cfe20393e7030e20e4d0
jack <giac290595@gmail.com>
2018-10-05 13:18:21 +0100

    added func which returns max target occurencies associates to each column value

--- Rule_Extractor.py
+++ Rule_Extractor.py
@@ -2,7 +2,7 @@
 
 def matrix_to_csv(file):
     file = open(file)
-    output = open ("output.csv", "w")
+    output = open("output.csv", "w")
     output.write("age,sp,ast,tpr,lens\n")
     for line in file:
         output.write(line.split()[1])
@@ -23,7 +23,6 @@ def labeler(v1, v2, val_dict, string):
             val_dict[string][i] = v1
         else: val_dict[string][i] = v2
     return val_dict
-
 
 
 def multi_labeler(val_dict, code, v1, v2, v3 ,v4, v5 ,v6):
@@ -66,31 +65,57 @@ def values_to_dictionary(file):
         values_dict["tear_production"].append(int(line.split()[3]))
         values_dict["lenses_target"].append(int(line.split()[4]))
 
-    # print(values_dict["age"])
-
     return values_dict
 
 
-def different_values(list):
+def different_values(lista):
     values = []
-    for el in list:
-        values.append(set(list[el]))
+    for el in lista:
+        values.append(set(lista[el]))
     return values
+
+
+def max_occ_per_val (ditt):
+
+    max_per_val_attr = max(ditt, key=ditt.get)
+    return max_per_val_attr
+
+
+def attrib_error(values_dict, solo_values, attrib):
+    total_attrib_error = 0
+    values_errors = []
+#    for i in solo_values[attrib]:
+ #       for el in range(0, len(values_dict[attrib])):
+  #          err = 0
+   #         piu_freq = max(values_dict[attrib], key=values_dict[attrib].count)
+    #        print(piu_freq)
+     #       values_errors.append(err)
+
+
+    return total_attrib_error
 
 
 def main():
 
+    total_error = dict()
     values_dict = values_to_dictionary('lenses.txt')
     target_col = values_dict.__getitem__("lenses_target")
     values_dict.__delitem__("lenses_target")
-
+    single_values_dict = dict()
 
     for code in range(1, 5):
-        print("hello")
         attribs = multi_labeler(values_dict, code, "myope", "hypermetrope", "non astigmatic", "astigmatic", "normal", "reduced")
 
-    solo_values = different_values(attribs)
+    solo_values_set = different_values(attribs)
 
-    print(attribs)
+    for key in (values_dict.keys()):
+        single_values_dict[key] = set(values_dict[key])
+
+    for attrib in single_values_dict:
+        total_error[attrib] = 0
+        total_error[attrib] = attrib_error(values_dict, single_values_dict, attrib)
+
+    # print(total_error)
+
 
 main()
